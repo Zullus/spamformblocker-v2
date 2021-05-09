42741ecdc9449d0343850c0c5df6e1dc9f562101
Edson <edson.santos@i9xp.com.br>
2021-05-09 03:14:52 -0300

    Iniciando a segunda versão

---
+++ database/migrations/2021_05_09_060218_create_badmails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBadmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_bademails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->integer('ativo')->default(1);
+            $table->string('ip')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('table_bademails');
+    }
+}
+
---
+++ database/migrations/2021_05_09_060554_create_table_accesslog.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableAccesslog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_accesslogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip');
+            $table->string('reverseip');
+            $table->string('useragent');
+            $table->string('blockemail');
+            $table->string('action');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_accesslog');
+    }
+}
